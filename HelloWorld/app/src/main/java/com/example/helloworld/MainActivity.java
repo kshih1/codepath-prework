7db07ea45a05e7dab54c8020cbac3a15105d22ac
kshih1 <54177962+kshih1@users.noreply.github.com>
2022-02-04 16:55:08 -0600

    Added the prework files to repository

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // User can tap a button to change the text color of the label
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //change text color of the label
+                ((TextView) findViewById(R.id.text)).setTextColor(getResources().getColor(R.color.yellow));
+            }
+        });
+    }
+}
